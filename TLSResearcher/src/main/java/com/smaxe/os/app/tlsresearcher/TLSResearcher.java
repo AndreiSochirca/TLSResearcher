e2e1a4289f85842365dd9fdf84a5b057b33e9049
Andrei Sochirca <andrei.sochirca@gmail.com>
2023-08-11 00:50:46 +0300

    Version 0.0.1: Application start point. Console application welcomes the world!

---
+++ TLSResearcher/src/main/java/com/smaxe/os/app/tlsresearcher/TLSResearcher.java
@@ -0,0 +1,16 @@
+package com.smaxe.os.app.tlsresearcher;
+
+/**
+ * <code>TLSResearcher</code> - TLS Researcher entry point.
+ */
+public final class TLSResearcher extends Object {
+
+	/**
+	 * Application entry point.
+	 * 
+	 * @param args
+	 */
+	public static void main(final String... args) {
+		System.out.println("Hello, World! I am TLS Researcher");
+	}
+}
